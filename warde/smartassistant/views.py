6aee81d34f63cffd37d44a15071c9b6c93a7153e
Marcin Kolny <marcin.kolny@gmail.com>
2015-02-14 16:31:34 +0100

    eremeref all previous data (use git log to view previous sources)

--- warde/smartassistant/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from core.models import UserInfo, Style, Gender
-
-# Create your views here.
-
-
-def assistant_indesx(request):
-    return HttpResponse("hej")
